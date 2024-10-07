c1d191950652d40685b192de04e5e85a5428b254
luckys-lnz <lalucky22@gmail.com>
2024-10-07 14:31:44 +0000

    1. More mapping

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,10 @@
+const getListStudentIds = (students) =>{
+    // check if student is an array and not empty
+    if (!Array.isArray(students) || students.length === 0){
+        return []
+    }
+    // return student ids in the list
+    return students.map(student=>student.id)
+}
+
+export default getListStudentIds
