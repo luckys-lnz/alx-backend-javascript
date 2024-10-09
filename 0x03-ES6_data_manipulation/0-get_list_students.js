b65565532a07db0cd0fbb07edee9ecb68bc8e91a
luckys-lnz <lalucky22@gmail.com>
2024-10-09 00:28:17 +0000

    0. Basic list of objects

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,14 +1,7 @@
-const getListStudents = () => {
-
-  // create a student array of objects
-  const students = [
-    { id: "1", firstName: "Guillaume", location: "San Francisco" },
-    { id: "2", firstName: "James", location: "Columbia" },
-    { id: "5", firstName: "Serena", location: "San Francisco" },
+export default function getListStudents() {
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
   ];
-  
-  // return students
-  return students
-};
-
-export default getListStudents;
+}
