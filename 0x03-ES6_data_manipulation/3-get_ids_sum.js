4d90041e1411e70ca92c96811be0ca8a33565530
luckys-lnz <lalucky22@gmail.com>
2024-10-09 00:28:55 +0000

    3. Reduce

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,6 +1,4 @@
-const getStudentIdsSum = (students) => {
+export default function getStudentIdsSum(students) {
   // sum student ids using reduce()
   return students.reduce((sum, student) => sum + Number(student.id), 0);
-};
-
-export default getStudentIdsSum;
+}
