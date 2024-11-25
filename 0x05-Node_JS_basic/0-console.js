1d6046f98e6c0f668f16f4c2d0782d3058134657
luckys-lnz <lalucky22@gmail.com>
2024-11-25 13:31:29 +0000

    print stdin

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Function to display a message in the standard output.
+ * @param {string} message - The message to be printed.
+ */
+
+function displayMessage(message){
+    console.log(message);
+}
+
+module.exports = displayMessage;
