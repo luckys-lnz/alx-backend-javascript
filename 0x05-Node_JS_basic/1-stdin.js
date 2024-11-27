ce0bac3706b13def357378039797b282e291b48e
luckys-lnz <lalucky22@gmail.com>
2024-11-27 09:55:56 +0000

    readable

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,15 +1,13 @@
-process.stdout.write("Welcome to Holberton School, what is your name?\n");
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-// Listen for user input
-process.stdin.on("data", (data) => {
-  const name = data.toString().trim();
-  process.stdout.write(`Your name is: ${name}\n`);
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
 
-  // Close program
-  process.exit(0);
+  if (name) {
+    process.stdout.write(`Your name is: ${name}`);
+  }
 });
 
-// Handle program exit
-process.on("exit", () => {
-  console.log("This important software is now closing");
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
